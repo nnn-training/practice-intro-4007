672529f809b5c1b18ce0fabe4bdd7870fa69ed31
Fuuchin <denkisutandokese@outlook.jp>
2023-01-24 19:22:18 +0900

    「移動」を実装（ついでに「点滅」を実装した）

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,22 @@
 import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
+const flashingButton = $('#flashing-button');
 
 scalingButton.on('click', () => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingButton.on('click', () => {
+  block.animate({ marginLeft: '500px' }, 500);
+  block.animate({ marginLeft: '20px' }, 1000);
+});
+
+flashingButton.on('click', () => {
+  block.animate({ opacity: 0 }, 100);
+  block.animate({ opacity: 1 }, 100);
+  block.animate({ opacity: 0 }, 100);
+  block.animate({ opacity: 1 }, 100);
 });
